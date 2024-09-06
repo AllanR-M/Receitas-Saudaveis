be25d396a392093b3683221229f505601055d91d
AllanR-M <allanrj.rodrigues@gmail.com>
2024-09-06 09:51:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,25 +2,45 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
   
+    let campoPesquisa = document.getElementById("campo-pesquisa").value 
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<P>Nada foi encontrado</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descrição = "";
+    let tags = "";
+    
   
     // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array)
-    for (let dado of dados) {
-      // Constrói o HTML para cada item do resultado da pesquisa,
-      // utilizando template literals para facilitar a concatenação
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descrição}</p>
-          <a href="${dado.receita}" target="_blank">Clique e confira</a>
-        </div>`;
+    for (let dado of dados) { 
+        titulo = dado.titulo.toLocaleLowerCase()
+        descrição = dado.descrição.toLocaleLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        // se titilo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descrição.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            //// Constrói o HTML para cada item do resultado da pesquisa,
+            resultados += `
+            <div class="item-resultado">
+              <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descrição}</p>
+              <a href="${dado.receita}" target="_blank">Clique e confira</a>
+            </div>`;
+        }     
     }
   
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
     // Atribui o HTML gerado para a seção de resultados
     section.innerHTML = resultados;
   }
-
-
